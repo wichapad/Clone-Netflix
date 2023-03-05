984394bbfa559cd186d1efe963e7b08065a9927b
Noah <guy_wichapad@hotmail.com>
2023-03-05 17:19:50 +0700

    fetch api movies

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -12,16 +12,66 @@
 // }
 // window.addEventListener('scroll',changeBg)
 
-const slider = document.querySelector(".sliderMoviesBox");
-
-var scrollPerclick;
-
+const slider = document.querySelector(".sliderMovies");
 
 showMovieData();
 
+function showMovieData() {
+    const api_key = "20a3b2db64569377dafa9b2c6094e31f"; //API Key
+    const urls = [
+        `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22`,
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`,
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}`,
+        `https://api.themoviedb.org/3/discover/movie/?api_key=${api_key}&sort_by=popularity.desc`,
+        `https://api.themoviedb.org/3/discover/movie/?api_key=${api_key}&with_genres=35&with_cast=23659&sort_by=revenue.desc`,
+        `https://api.themoviedb.org/3/discover/movie/?api_key=${api_key}&with_genres=878&with_cast=500&sort_by=vote_average.desc`
+    ];
+
+    urls.map(url => fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results)
+            const movies = data.results;
+            const sliderMoviesBox = document.createElement('div');
+            sliderMoviesBox.classList.add('sliderMoviesBox')
+
+            const BoxMovie = document.createElement('div');
+            BoxMovie.classList.add("boxmovies")
+
+            const anchorLeft = document.createElement('a');
+            anchorLeft.classList.add("switchLeft", "sliderButton");
+            anchorLeft.onclick = function() { sliderScrollLeft(); };
+            anchorLeft.textContent = "<";
+
+            const anchorRight = document.createElement('a');
+            anchorRight.classList.add("switchRight", "sliderButton");
+            anchorRight.onclick = function() { sliderScrollRight(); };
+            anchorRight.textContent = ">";
+
+            movies.forEach(function (movie) {
+
+                const poster = document.createElement('img');
+                poster.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+
+                BoxMovie.appendChild(poster);
+                sliderMoviesBox.appendChild(BoxMovie);
+                sliderMoviesBox.appendChild(anchorLeft);
+                sliderMoviesBox.appendChild(anchorRight);
+                slider.appendChild(sliderMoviesBox);
+
+            });
+        })
+    )
+    scrollPerclick = 450;
+}
+
+const scrollslider = document.querySelector(".sliderMoviesBox");
+
+var scrollPerclick;
+
 var scrollAmount = 0;
-function sliderScrollLeft() {
-    slider.scrollTo({
+function sliderScrollLeft(){
+    scrollslider.scrollTo({
         top: 0,
         left: (scrollAmount -= scrollPerclick),
         behavior: "smooth"
@@ -34,33 +84,12 @@ function sliderScrollLeft() {
 
 function sliderScrollRight() {
     if (scrollAmount <= slider.scrollWidth - slider.clientWidth) {
-        slider.scrollTo({
+        scrollslider.scrollTo({
             top: 0,
             left: (scrollAmount += scrollPerclick),
             behavior: "smooth"
         })
     }
-}
-
-function showMovieData() { //async
-    // const api_key = "20a3b2db64569377dafa9b2c6094e31f"; //API Key
-
-    // var result = await axios.get(
-    //     "https://api.themoviedb.org/3/discover/movie?api_key=" + //Example API Request
-    //     api_key +
-    //     "&sort_by=popularity.desc" //Discover Examples
-    // );
-
-    // result = result.data.results;
-
-    // result.map(function (cur, index) {
-    //     slider.insertAdjacentHTML(
-    //         "beforeend",
-    //         `<img class="img-${index} slider-img" src="https://image.tmdb.org/t/p/w185/${cur.poster_path}">`
-    //     )
-    // });
-
-    scrollPerclick = 450;
 }
 
 
