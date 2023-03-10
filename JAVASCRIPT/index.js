cba5009f8367811bccf8543c64e465db8cecc196
Noah <guy_wichapad@hotmail.com>
2023-03-10 14:52:40 +0700

    ำกระ

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -12,6 +12,13 @@ function scrollNavbar(){
 // hover browse show list-navbar mobile screen
  
 
+
+
+
+
+
+
+
 // Movies slider
 const slider = document.querySelector(".sliderMovies");
 
@@ -27,6 +34,9 @@ function showMovieData() {
         `https://api.themoviedb.org/3/discover/movie/?api_key=${api_key}&with_genres=35&with_cast=23659&sort_by=revenue.desc`,
         `https://api.themoviedb.org/3/discover/movie/?api_key=${api_key}&with_genres=878&with_cast=500&sort_by=vote_average.desc`
     ];
+    const title = [
+        
+    ]
 
     urls.map(url => fetch(url)
         .then(res => res.json())
@@ -41,30 +51,29 @@ function showMovieData() {
             BoxMovie.classList.add("boxmovies")
 
             const anchorLeft = document.createElement('a');
-            anchorLeft.classList.add("switchLeft", "sliderButton");
-            anchorLeft.textContent = "<";
+            anchorLeft.classList.add("switchLeft", "sliderButton","fa-solid","fa-chevron-left");
+            anchorLeft.style.display ="flex";
 
             const anchorRight = document.createElement('a');
-            anchorRight.classList.add("switchRight", "sliderButton");
-            anchorRight.textContent = ">";
+            anchorRight.classList.add("switchRight", "sliderButton","fa-solid","fa-chevron-right");
+            anchorRight.style.display ="flex";
 
-            // const genre = document.createElement("h2");
-            // genre.textContent = "trending Now";
+            const title = document.createElement("h2");
+            title.classList.add("title")
+            title.textContent = "trending Now";
 
             movies.forEach(function (movie) {
 
                 const poster = document.createElement('img');
                 poster.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
 
+                slider.appendChild(title);
                 slider.appendChild(sliderMoviesBox);
-                // slider.appendChild(genre);
                 sliderMoviesBox.appendChild(BoxMovie);
+                BoxMovie.appendChild(poster);
                 sliderMoviesBox.appendChild(anchorLeft);
                 sliderMoviesBox.appendChild(anchorRight);
-                BoxMovie.appendChild(poster);
-
-
-
+                
             });
         })
     )
